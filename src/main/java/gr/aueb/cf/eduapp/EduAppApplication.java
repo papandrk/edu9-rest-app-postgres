c729220707e39b3556d3154f0c17be84bde3c3ed
papandrk <papandrk@gmail.com>
2026-06-16 02:12:33 +0300

    feat: add model entities

--- src/main/java/gr/aueb/cf/eduapp/EduAppApplication.java
+++ src/main/java/gr/aueb/cf/eduapp/EduAppApplication.java
@@ -2,8 +2,10 @@ package gr.aueb.cf.eduapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class EduAppApplication {
 
 	public static void main(String[] args) {
