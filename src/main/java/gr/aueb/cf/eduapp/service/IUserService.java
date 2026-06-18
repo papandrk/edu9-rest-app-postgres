0bcab23a9d5fad08b8db6716f1e009dd158a2b66
papandrk <papandrk@gmail.com>
2026-06-18 21:12:30 +0300

    chore: add entity not found exception to user service interface

--- src/main/java/gr/aueb/cf/eduapp/service/IUserService.java
+++ src/main/java/gr/aueb/cf/eduapp/service/IUserService.java
@@ -2,6 +2,7 @@ package gr.aueb.cf.eduapp.service;
 
 import gr.aueb.cf.eduapp.core.exception.EntityAlreadyExistsException;
 import gr.aueb.cf.eduapp.core.exception.EntityInvalidArgumentException;
+import gr.aueb.cf.eduapp.core.exception.EntityNotFoundException;
 import gr.aueb.cf.eduapp.dto.UserInsertDTO;
 import gr.aueb.cf.eduapp.dto.UserReadOnlyDTO;
 
@@ -12,6 +13,6 @@ public interface IUserService {
     UserReadOnlyDTO saveUser(UserInsertDTO userInsertDTO)
             throws EntityAlreadyExistsException, EntityInvalidArgumentException;
 
-    UserReadOnlyDTO getUserByUUID(UUID uuid);
-    UserReadOnlyDTO getUserByUUIDDeletedFalse(UUID uuid);
+    UserReadOnlyDTO getUserByUUID(UUID uuid) throws EntityNotFoundException;
+    UserReadOnlyDTO getUserByUUIDDeletedFalse(UUID uuid) throws EntityNotFoundException;
 }
