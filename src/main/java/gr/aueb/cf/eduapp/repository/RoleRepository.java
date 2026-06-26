cc5609f1bc2e2577423c3d1ef798e9c94837b53e
papandrk <papandrk@gmail.com>
2026-06-26 05:32:37 +0300

    fix: typo in method name

--- src/main/java/gr/aueb/cf/eduapp/repository/RoleRepository.java
+++ src/main/java/gr/aueb/cf/eduapp/repository/RoleRepository.java
@@ -7,5 +7,5 @@ import java.util.List;
 
 public interface RoleRepository extends JpaRepository<Role, Long> {
 
-    List<Role> FindAllByOrderByNameAsc();
+    List<Role> findAllByOrderByNameAsc();
 }
