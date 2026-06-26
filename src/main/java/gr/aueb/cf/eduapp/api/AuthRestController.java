dc79ebb56a1dda71586e2ba9dd50975e98a8b58e
papandrk <papandrk@gmail.com>
2026-06-26 18:12:33 +0300

    feat: added user authentication endpoint to api

--- src/main/java/gr/aueb/cf/eduapp/api/AuthRestController.java
+++ src/main/java/gr/aueb/cf/eduapp/api/AuthRestController.java
@@ -6,6 +6,8 @@ import gr.aueb.cf.eduapp.dto.AuthenticationResponseDTO;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -16,7 +18,8 @@ public class AuthRestController {
 
     private final AuthenticationService authenticationService;
 
-    public ResponseEntity<AuthenticationResponseDTO> authenticate(AuthenticationRequestDTO dto) {
+    @PostMapping("/authenticate")
+    public ResponseEntity<AuthenticationResponseDTO> authenticate(@RequestBody AuthenticationRequestDTO dto) {
         AuthenticationResponseDTO responseDTO = authenticationService.authenticate(dto);
         return  ResponseEntity.ok(responseDTO);
         // return new ResponseEntity<>(responseDTO, HttpStatus.OK); // effectively the same thing as the above line
--- src/main/java/gr/aueb/cf/eduapp/security/SecurityConfiguration.java
+++ src/main/java/gr/aueb/cf/eduapp/security/SecurityConfiguration.java
@@ -50,6 +50,7 @@ public class SecurityConfiguration {
                 .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(req -> req
                                 .requestMatchers(HttpMethod.POST, "/api/v1/users").permitAll()
+                                .requestMatchers(HttpMethod.POST, "/api/v1/auth/authenticate").permitAll()
                                 // .requestMatchers(HttpMethod.GET, "/api/v1/users/{uuid}").permitAll() // not sure if this works/is valid
                                 // .requestMatchers(HttpMethod.GET, "/api/v1/users/*").permitAll()
                                 .requestMatchers(HttpMethod.GET, "/api/v1/users/*").hasAuthority("VIEW_USER")
