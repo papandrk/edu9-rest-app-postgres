507b618ee11f4f834e4fcbeb4dcd466b5c857841
papandrk <papandrk@gmail.com>
2026-07-22 01:40:21 +0300

    chore: add clarifying comments

--- src/main/java/gr/aueb/cf/eduapp/mapper/Mapper.java
+++ src/main/java/gr/aueb/cf/eduapp/mapper/Mapper.java
@@ -10,6 +10,9 @@ import org.springframework.stereotype.Component;
 public class Mapper {
 
     public User mapToUserEntity(UserInsertDTO insertDTO) {
+
+        // TODO : doesn't insertDTO.roleId also need to be mapped in this method??? -> (guess at answer) no, because the role already exists and it is not created during user creation - the roleId must be used to retrieve the role via the RoleRepository (and for some reason that i don't know (perhaps it's just preference), that is to be done in the UserService, NOT here in the mapper)
+
         return new User(insertDTO.username(), insertDTO.password());
     }
 
@@ -37,6 +40,8 @@ public class Mapper {
         personalInfo.setMunicipalityOfRegistration(personalInfoDTO
                 .municipalityOfRegistration());
         teacher.setPersonalInfo(personalInfo);  // Set PersonalInfo entity on Teacher (one-way relation)
+
+        // TODO : doesn't dto.regionId also need to be mapped in this method??? -> (guess at answer) no, because the region already exists and it is not created during teacher creation - the regionId must be used to retrieve the regio via the RegionRepository (and for some reason that i don't know (perhaps it's just preference), that is to be done in the TeacherService, NOT here in the mapper)
 
         return teacher;
     }
