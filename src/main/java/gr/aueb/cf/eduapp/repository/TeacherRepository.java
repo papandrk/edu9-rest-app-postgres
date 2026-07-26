80c709b4335dd4b4438907e935ee5d294dc6e569
papandrk <papandrk@gmail.com>
2026-07-26 18:27:28 +0300

    chore: add clarifying comment

--- src/main/java/gr/aueb/cf/eduapp/repository/TeacherRepository.java
+++ src/main/java/gr/aueb/cf/eduapp/repository/TeacherRepository.java
@@ -6,10 +6,12 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 import java.util.UUID;
 
+// @Repository // not required, since we extend JpaRepository (if we didn't extend it, then we would need @Repository)
 public interface TeacherRepository extends JpaRepository<Teacher, Long>, JpaSpecificationExecutor<Teacher> {
 
     Optional<Teacher> findByUuid(UUID uuid);
