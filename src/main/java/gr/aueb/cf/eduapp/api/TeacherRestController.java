e89810682ca0ee10f7bbac2d0f2aa24dc26b1b3c
papandrk <papandrk@gmail.com>
2026-07-26 19:54:40 +0300

    chore: add save teacher controller

---
+++ src/main/java/gr/aueb/cf/eduapp/api/TeacherRestController.java
@@ -0,0 +1,52 @@
+package gr.aueb.cf.eduapp.api;
+
+import gr.aueb.cf.eduapp.core.exceptions.EntityAlreadyExistsException;
+import gr.aueb.cf.eduapp.core.exceptions.EntityInvalidArgumentException;
+import gr.aueb.cf.eduapp.core.exceptions.ValidationException;
+import gr.aueb.cf.eduapp.dto.TeacherInsertDTO;
+import gr.aueb.cf.eduapp.dto.TeacherReadOnlyDTO;
+import gr.aueb.cf.eduapp.service.ITeacherService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
+
+@RestController
+@RequestMapping("/api/v1/teachers")
+@RequiredArgsConstructor
+public class TeacherRestController {
+
+    private final ITeacherService teacherService;
+
+    @PostMapping
+    public ResponseEntity<TeacherReadOnlyDTO> saveTeacher(
+            @Valid @RequestBody TeacherInsertDTO teacherInsertDTO,
+            BindingResult bindingResult
+    ) throws EntityAlreadyExistsException, EntityInvalidArgumentException, ValidationException {
+
+        // teacherInsertValidator.validate(teacherInsertDTO, bindingResult);
+
+        if (bindingResult.hasErrors()) {
+            throw new ValidationException("Teacher", "Invalid teacher data", bindingResult);
+        }
+
+        TeacherReadOnlyDTO teacherReadOnlyDTO = teacherService.saveTeacher(teacherInsertDTO);
+
+        URI location = ServletUriComponentsBuilder
+                .fromCurrentRequest()
+                .path("/{uuid}")
+                .buildAndExpand(teacherReadOnlyDTO.uuid())
+                .toUri();
+
+        return ResponseEntity
+                .created(location)
+                .body(teacherReadOnlyDTO);
+    }
+}
