f7a8d601a0e8525e3b5d9dd7cf620d84e2324ece
papandrk <papandrk@gmail.com>
2026-07-28 01:54:11 +0300

    chore: add permissions for "update-teacher" URL to require EDIT_TEACHER authority

--- src/main/java/gr/aueb/cf/eduapp/security/SecurityConfiguration.java
+++ src/main/java/gr/aueb/cf/eduapp/security/SecurityConfiguration.java
@@ -52,7 +52,8 @@ public class SecurityConfiguration {
                                 .requestMatchers(HttpMethod.POST, "/api/v1/users").permitAll()
                                 .requestMatchers(HttpMethod.POST, "/api/v1/auth/authenticate").permitAll()
                                 .requestMatchers(HttpMethod.POST, "/api/v1/teachers").permitAll()
-                                .requestMatchers(HttpMethod.POST, "/api/v1/teachers/{uuid}/*").permitAll() // TODO: make only the endpoint with the uuid of current user accessible, unless they have ADMIN role
+                                .requestMatchers(HttpMethod.POST, "/api/v1/teachers/{uuid}/*").permitAll() // (for adding an amkaFile) TODO: make only the endpoint with the uuid of current user accessible, unless they have ADMIN role
+                                .requestMatchers(HttpMethod.PUT, "/api/v1/teachers/{uuid}").hasAuthority("EDIT_TEACHER")
                                 // .requestMatchers(HttpMethod.GET, "/api/v1/users/{uuid}").permitAll() // not sure if this works/is valid
                                 // .requestMatchers(HttpMethod.GET, "/api/v1/users/*").permitAll()
                                 .requestMatchers(HttpMethod.GET, "/api/v1/users/*").hasAuthority("VIEW_USER")
