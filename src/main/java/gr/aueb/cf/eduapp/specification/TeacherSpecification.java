3db37e76644a5ab372df8976cab19f25da0637f3
papandrk <papandrk@gmail.com>
2026-07-29 14:31:58 +0300

    fix: add null check to region specification

--- src/main/java/gr/aueb/cf/eduapp/specification/TeacherSpecification.java
+++ src/main/java/gr/aueb/cf/eduapp/specification/TeacherSpecification.java
@@ -34,15 +34,10 @@ public class TeacherSpecification {
                 cb.like(cb.lower(root.get("lastname")), "%" + lastname.toLowerCase() + "%");
     }
 
-    // // (commented out the below declaration because a more succinct declaration follows it right below - the check for if region is null is optional since spring boot will automatically call cb.conjunction in that case)
-    // private static Specification<Teacher> hasRegion(String region) {
-    //     return (root, query, cb) -> region == null ? cb.conjunction() :
-    //             cb.equal(cb.lower(root.get("region").get("name")), region.toLowerCase());
-    // }
-    private static Specification<Teacher> hasRegion(String region) {
-        return (root, query, cb) -> cb.equal(
-                cb.lower(root.get("region").get("name")), region.toLowerCase());
-    }
+     private static Specification<Teacher> hasRegion(String region) {
+         return (root, query, cb) -> region == null ? cb.conjunction() :
+                 cb.equal(cb.lower(root.get("region").get("name")), region.toLowerCase());
+     }
 
     private static Specification<Teacher> isDeleted(boolean deleted) {
         return (root, query, cb) -> cb.equal(root.get("deleted"), deleted);
